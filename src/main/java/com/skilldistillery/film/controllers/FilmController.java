07e33090a0d120d71b16d627bbc18907f95d98ae
Michael Ventura <m.ventura14@yahoo.com>
2024-04-05 18:23:51 -0500

    Started code complete

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,8 +1,36 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.film.data.DatabaseAccessor;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
+
+	@Autowired
+	private DatabaseAccessor dao;
+
+	@RequestMapping(path = { "", "/", "index", "index.do" })
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		return mv;
+
+	}
+
+	@RequestMapping(path = { "find.do" }, method = RequestMethod.GET)
+	public String findFilmById(@RequestParam("id") int id, Model model) {
+		Film film = dao.findFilmById(id);
+		model.addAttribute("film", film);
+		return "film";
+
+	}
 
 }
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>MVC Film</h1>
+
+	<form action="find.do">
+	
+	<label>Film id: <input type="text" name="id"></label>
+			
+			<input type="submit" name="click me">
+		
+	</form>
+
+</body>
+</html>
